305aa0b87264d368f51a3d0bf87783ded7a84cc6
tuxdevelop <tuxdevelop@gmail.com>
2015-09-28 19:25:18 +0200

    updated Demo applications

--- spring-batch-lightmin-samples/address-migrator-war/src/main/java/org/tuxdevelop/spring/batch/lightmin/address_migrator/configuration/AddressPrinterJobConfiguration.java
+++ spring-batch-lightmin-samples/address-migrator-war/src/main/java/org/tuxdevelop/spring/batch/lightmin/address_migrator/configuration/AddressPrinterJobConfiguration.java
@@ -7,11 +7,15 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.tuxdevelop.spring.batch.lightmin.address_migrator.domain.Address;
 import org.tuxdevelop.spring.batch.lightmin.address_migrator.domain.ProcessingState;
 import org.tuxdevelop.spring.batch.lightmin.address_migrator.persistence.dao.AddressDAO;
@@ -27,6 +31,12 @@ public class AddressPrinterJobConfiguration {
     private static final String SELECT_ADDRESS_QUERY = "SELECT batch_task_id FROM batch_task_address WHERE processing_state = " +
             ProcessingState.SUCCESS;
 
+    private static final String SELECT_BATCH_TASK_ADDRESS_TO_DELETE_QUERY = "SELECT batch_task_id FROM batch_task_address " +
+            "WHERE processing_state = " + ProcessingState.PRINTED;
+
+    private static final String DELETE_BATCH_TASK_ADDRESS_STATEMENT = "DELETE FROM batch_task_address WHERE " +
+            "batch_task_id = :batch_task_id";
+
     @Autowired
     private DataSource dataSource;
 
@@ -41,6 +51,7 @@ public class AddressPrinterJobConfiguration {
         return jobBuilderFactory
                 .get("addressPrinterJob")
                 .start(addressPrinterStep())
+                .next(addressBatchTaskDeletionJob())
                 .build();
     }
 
@@ -54,6 +65,16 @@ public class AddressPrinterJobConfiguration {
                 .writer(addressPrinterWriter())
                 .build();
     }
+
+    public Step addressBatchTaskDeletionJob() throws Exception {
+        return stepBuilderFactory
+                .get("addressBatchTaskDeletionStep")
+                .<Long, Long>chunk(1)
+                .reader(addressBatchTaskDeletionReader())
+                .writer(addressBatchTaskDeletionWriter())
+                .build();
+    }
+
 
     @Bean
     public ItemProcessor<Long, Address> addressPrinterProcessor() {
@@ -86,12 +107,39 @@ public class AddressPrinterJobConfiguration {
     }
 
     @Bean
+    public JdbcBatchItemWriter<Long> addressBatchTaskDeletionWriter() {
+        final JdbcBatchItemWriter<Long> writer = new JdbcBatchItemWriter<Long>();
+        writer.setDataSource(dataSource);
+        writer.setSql(DELETE_BATCH_TASK_ADDRESS_STATEMENT);
+        writer.setItemSqlParameterSourceProvider(new ItemSqlParameterSourceProvider<Long>() {
+            @Override
+            public SqlParameterSource createSqlParameterSource(Long batchTaskId) {
+                final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+                mapSqlParameterSource.addValue("batch_task_id", batchTaskId);
+                return mapSqlParameterSource;
+            }
+        });
+        return writer;
+    }
+
+    @Bean
     public JdbcCursorItemReader<Long> addressPrinterReader() throws Exception {
         final JdbcCursorItemReader<Long> reader = new JdbcCursorItemReader<Long>();
         reader.setSql(SELECT_ADDRESS_QUERY);
         reader.setRowMapper(new SingleColumnRowMapper<Long>());
         reader.setDataSource(dataSource);
         reader.setMaxRows(100);
+        reader.afterPropertiesSet();
+        return reader;
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Long> addressBatchTaskDeletionReader() throws Exception {
+        final JdbcCursorItemReader<Long> reader = new JdbcCursorItemReader<Long>();
+        reader.setSql(SELECT_BATCH_TASK_ADDRESS_TO_DELETE_QUERY);
+        reader.setRowMapper(new SingleColumnRowMapper<Long>());
+        reader.setDataSource(dataSource);
+        reader.setMaxRows(50);
         reader.afterPropertiesSet();
         return reader;
     }
