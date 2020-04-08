a401a1e6d5bdcaa1beafb3728c621782d8e98d06
marbecker <marcel.becker@1und1.de>
2020-04-08 15:55:35 +0200

    added drop and create scripts for server scheduler jdbc repository

--- spring-batch-lightmin-server/spring-batch-lightmin-server-scheduler/spring-batch-lightmin-server-scheduler-repository/spring-batch-lightmin-server-scheduler-repository-jdbc/src/main/resources/org/tuxdevelop/spring/batch/lightmin/server/scheduler/repository/scheduler_schema_postgres.sql
+++ spring-batch-lightmin-server/spring-batch-lightmin-server-scheduler/spring-batch-lightmin-server-scheduler-repository/spring-batch-lightmin-server-scheduler-repository-jdbc/src/main/resources/org/tuxdevelop/spring/batch/lightmin/server/scheduler/repository/scheduler_schema_postgres.sql
@@ -1,27 +1,30 @@
 CREATE TABLE SCHEDULER_CONFIGURATION
 (
-    id                   NUMERIC IDENTITY PRIMARY KEY NOT NULL,
+    id                   SERIAL       NOT NULL,
     application_name     VARCHAR(255) NOT NULL,
     job_name             VARCHAR(255) NOT NULL,
-    configuration_status VARCHAR(255) NOT NULL
+    configuration_status VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE SCHEDULER_CONFIGURATION_VALUE
 (
-    id                         NUMERIC IDENTITY PRIMARY KEY NOT NULL,
-    scheduler_configuration_id NUMERIC      NOT NULL,
+    id                         SERIAL       NOT NULL,
+    scheduler_configuration_id BIGINT       NOT NULL,
     type                       VARCHAR(255) NOT NULL,
     value                      VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (scheduler_configuration_id) REFERENCES SCHEDULER_CONFIGURATION (id)
 );
 
 CREATE TABLE SCHEDULER_EXECUTION
 (
-    id                         NUMERIC IDENTITY PRIMARY KEY NOT NULL,
-    scheduler_configuration_id NUMERIC   NOT NULL,
+    id                         SERIAL    NOT NULL,
+    scheduler_configuration_id BIGINT    NOT NULL,
     next_fire_time             TIMESTAMP NOT NULL,
     execution_count            INT       NOT NULL DEFAULT 0,
     state                      INT       NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (scheduler_configuration_id) REFERENCES SCHEDULER_CONFIGURATION (id)
 );
 
